6480987b49b07cdfb276a7edfd16fd5e4ad2a47c
Rohan2596 <rohankadam662@gmail.com>
2020-10-23 16:25:47 +0530

    ADDED:MiddleWare and Mongoose Database connection.

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 const express=require('express');
 const { response } = require('express');
 const app=express();
+const mongoose=require('mongoose');
+
+//middleWare
+app.use('/post',()=>{
+    console.log("this is a middle ware");
+    
+})
+
 //routes
 app.get("/",(request,response)=>{
     response.send("Welcome to Node Js.");
@@ -8,6 +16,9 @@ app.get("/",(request,response)=>{
 app.get("/post",(request,response)=>{
     response.send("We are on posts.");
 });
-
+//Connect to DB
+mongoose.connect('http://localhost:27017/appDB',()=>
+console.log('Connected to DB!')
+);
 //listening to port
 app.listen(3000);
